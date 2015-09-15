889a03f37e1ecb7039904120d7cbad59d6480bfa
Kristjan <nigathr@gmail.com>
2015-09-15 22:16:36 +0200

    Added html to main.php

--- main.php
+++ main.php
@@ -3,5 +3,15 @@ $username = "Stranger";
 if(isset($_COOKIE["user"])){
 	$username = $_COOKIE["user"];
 }
-echo("Hello ".$username."!");
 ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="ISO-8859-1">
+		<title>Hello <?php echo($username);?>!</title>
+	</head>
+	
+	<body>
+	
+	</body>
+</html>
